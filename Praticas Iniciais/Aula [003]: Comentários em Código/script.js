441063580dcde9eb14b117f688da8905c8c513a0
Vitor <vitorvvc2005@gmail.com>
2024-01-10 15:11:39 -0300

    Adicionando aula [003] Comentários em Código

---
+++ Praticas Iniciais/Aula [003]: Comentários em Código/script.js
@@ -0,0 +1,17 @@
+// Escrever um comentário
+console.log("Olá mundo"); // Aqui tem outro comentário
+console.log("Linha 1");
+// Conentário
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+// Vai ser ignorado
+
+/* Comentário de bloco 
+Linha de comentário 2 */
+
+/**
+ * Um titulo
+ * 
+ * Lorem ipsum
+ */
