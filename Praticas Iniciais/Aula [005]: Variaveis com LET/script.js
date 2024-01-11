6887c3cd8e81fa14d020d6c0bbfcf14edc830171
Vitor <vitorvvc2005@gmail.com>
2024-01-11 08:43:26 -0300

    Subindo aula [005] Variaveis com LET

---
+++ Praticas Iniciais/Aula [005]: Variaveis com LET/script.js
@@ -0,0 +1,32 @@
+// let nome = "Henrique"; // Let --> Criar variaveis ( Pode VARIAR !!)
+// var nomeDois = nome; // Var --> "Let antiga"
+
+// console.log(nome, "nasceu em 1984.");
+// console.log("em 2000", nome ,"conheceu maria");
+// console.log(nome, "casou-se com Maria em 2012");
+// console.log("Maria teve um filho com " , nome , "em 2015");
+// console.log("O filho de", nome , "se chama Eduardo");
+
+// let nome;
+// nome = 'Qualquer nome';
+// console.log(nome);
+// nome = 'Otávio';
+// console.log(nome);
+
+// Não podemos criar variaveis com palavras reservadas \\
+// ex: let console;
+// Variáveis precisam ter nome significativos \\
+// ex: let n = 'João'; ( Errado !)
+// ex: let nome = 'João'; ( Correto )
+
+// Não pode começar o nome de uma variavel com NUMERO \\
+// ex: let 1nome;
+
+// Nome das variaveis não podem contar espaços ou traços \\
+// Padrão camelCase
+// ex: let nome-Client; ( Errado )
+// ex: let nome Client; ( Errado )
+// ex: let nomeCliente; ( Correto )
+
+// Case sensitive !! \\
+// ex: nomeCliente != nomecliente;
