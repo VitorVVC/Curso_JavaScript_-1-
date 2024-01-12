7b9400940198f6043bd16c621ef90c07fd224069
Vitor <vitorvvc2005@gmail.com>
2024-01-12 16:30:00 -0300

    Subindo aula [006] Constantes com CONST

---
+++ Praticas Iniciais/Aula [006]: Constantes com CONST/script.js
@@ -0,0 +1,15 @@
+                        // Regras sobre const \\
+// [1]: Não podemos criar constantes com palavras reservadas da linguagem JS (Exemplo: const let = n;)
+// [2]: Constantes precisam ter nomes significativos (Nada de 'N', 'X', 'Y' e etc.)
+// [3]: Não podemos criar o nome de uma constante iniciando-o com numeros.
+// [4]: Não podemos atribuir espaços ou traços ao nome de uma constante.
+// [5]: Const também utiliza o formato camelCase.
+// [6]: Const também utiliza Case-sensitive.
+// [7]: Não podemos modificar o valor de uma constante.
+// [8]: Não utilize VAR, utilize const.
+// [9]: Diferente de LET, não podemos fazer (const nome;). Para atribuir depos.
+
+const nome = "João";
+console.log(typeof nome); // Para descobrir qual o tipo de qualquer variavel do progama 
+
+
