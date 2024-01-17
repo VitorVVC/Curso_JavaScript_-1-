a87e3cb2af4aafd8dc51182bb6990f263109485d
Vitor <vitorvvc2005@gmail.com>
2024-01-17 17:55:40 -0300

    Subindo aula [007] Exercicio proposto

---
+++ Praticas Iniciais/Aula [007]:  Exercicio com LET e CONST/script.js
@@ -0,0 +1,13 @@
+// Nesta aula faremos um alguns exercicios sobre LET & Const
+
+// [Exericio [1]]
+// Temos que exibir: "Vitor Vargas Cardoso tem 18 anos, pesa 70kg e tem 1.75 de altura. Seu IMC é: 25.77554524241"
+
+const nome = "Vitor";
+const sobrenome = "Vargas Cardoso"
+const idade = 18;
+const peso = 70.5;
+const altura = 1.76;
+
+let IMC;
+let anoNascimento;
