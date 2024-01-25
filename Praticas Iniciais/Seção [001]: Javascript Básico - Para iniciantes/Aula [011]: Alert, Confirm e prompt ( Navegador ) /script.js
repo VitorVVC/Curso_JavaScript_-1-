dd3c633bce7eaba9e602f305e94774cbdd1969c2
Vitor <vitorvvc2005@gmail.com>
2024-01-25 16:35:56 -0300

    Subindo aula [011]

---
+++ Praticas Iniciais/Seção [001]: Javascript Básico - Para iniciantes/Aula [011]: Alert, Confirm e prompt ( Navegador ) /script.js
@@ -0,0 +1,9 @@
+// Isto alerta o usuario com algo necessário. Caso o usuario clique no cancelar retornar um "boolean nome = false;" se clicar no ok será "boolean nome = true;" ( Porém a função em si não retorna nada )
+// alert("Com a nossa mensagem")
+
+// Esta função é como um "alert escrito", Onde nos damos ao usuario uma tela para escrever algo que será retornado em String.
+// prompt("Digita ai chefe");
+
+// Está função assim como um alert, porém ela pede ao usuario confirmar uma ação, assim retornando true ou false.
+// confirm("Deseja realmente apagar?")
+// const confirmar = confirm("Deseja realmente apagar?");
