b9f7a3b74e8764df85d4fafd6f9f2329922cd138
Vitor <vitorvvc2005@gmail.com>
2024-01-25 16:46:55 -0300

    Subindo exercicio aula 011

--- Praticas Iniciais/Seção [001]: Javascript Básico - Para iniciantes/Aula [011]: Alert, Confirm e prompt ( Navegador ) /script.js
+++ Praticas Iniciais/Seção [001]: Javascript Básico - Para iniciantes/Aula [011]: Alert, Confirm e prompt ( Navegador ) /script.js
@@ -7,3 +7,15 @@
 // Está função assim como um alert, porém ela pede ao usuario confirmar uma ação, assim retornando true ou false.
 // confirm("Deseja realmente apagar?")
 // const confirmar = confirm("Deseja realmente apagar?");
+
+
+// EXERCICIO !! \\
+// Recer dois prompts e realizar uma conta de soma para dois numeros e fazer um alert com o resultado
+
+let num1 = prompt("Digite o primeiro numero: ") ;
+let num2 = prompt("Digite o segundo numero: ");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O resultado de sua conta é: ${num1 + num2}`);
