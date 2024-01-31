c87e337dc09b41373c5893fa5f37054aa0c572f1
Vitor <vitorvvc2005@gmail.com>
2024-01-31 15:54:26 -0300

    subindo aula sobre Arrays [Básico]

---
+++ Praticas Iniciais/Seção [001]: Javascript Básico - Para iniciantes/Aula [017]: Arrays ( Básico )/script.js
@@ -0,0 +1,16 @@
+// V -> 0 I -> 1 ...
+// const nome = 'Vitor Vargas';
+// Luiz --> 0 | Neymar --> 1 | Loucura --> 2 ...
+const alunos = ['Luiz Gonzaga','Neymar Junior','Loucura'];
+// alunos[0] = 'Eduardo'; // Substituir
+// alunos[3] = 'Novo aluno' // Forma de adicionar, meio ruim mas existe.
+// alunos[alunos.length] = 'Novo aluno'; // Forma de adicionar, mais eficiente.
+// alunos.push('Novo aluno'); // Melhor forma !!! We love push <3. (Porém so adiciona ao final do array)
+// alunos.unshift('Novo aluno'); // Melhor forma de adicionar, porém somente ao primeiro indice
+// alunos.pop() // Remove do final
+// alunos.shift() // Remove do inicio
+// delete alunos[1]; // Remove o indice passado, porém terá um elemento vazio
+// console.log(alunos[50]); // Ele até printa, porém será um undefined
+// console.log(alunos.slice(0,2)); // Eu escolho elemento X até Y
+// console.log(alunos.slice(0,-2)); // Eu escolho elemento X até Y ( De trás pra frente )
+// console.log(alunos instanceof Array); // Verifica se alunos é um Array
