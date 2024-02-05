142ab000dbd12cf8836a1a4f603035d8dbd6c21e
Vitor <vitorvvc2005@gmail.com>
2024-02-05 15:51:40 -0300

    Adicionando nova secão e aula [001], Operadores de comparacão

---
+++ Praticas Iniciais/Seção [002]: Lógica de progamação/Aula [001]: Operadores de compração/script.js
@@ -0,0 +1,19 @@
+/* Operadores de comparação:
+    > ( Maior que )
+    >= ( Maior que ou igual a )
+    < ( Menor que)
+    <= ( Menor que ou igual a)
+    == ( Igualdade )(Valor) {( = Atribuição )}
+    === ( Igualdade estrita ) (Valor e tipo)
+    != ( Diferente ) (Valor)
+    !== ( Diferenteg estrito ) (Valor e tipo)
+*/
+
+// Sempre retorna um valor boolean ( true ou false ).
+// Porque usar "==="
+// const num1 = 10;
+// const num2 = '10';
+// const comparação = num1 === num2; // Use "===" ao invés de "==" pois caso tenha um caso como esse retornará true, oque não desejamos.
+// console.log(comparação);
+// Porque usar "!==": A mesma coisa acima, o "!=" compara apenas oque há entre o valor, e não tipo.
+
