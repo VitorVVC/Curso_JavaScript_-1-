2d88b1e0223d99b4b5ec08e0be9bd5918e47211b
Vitor <vitorvvc2005@gmail.com>
2024-02-09 15:03:44 -0300

    Subindo segunad parte de if else

--- Praticas Iniciais/Seção [002]: Lógica de progamação/Aula [004] IF - ELSE/script.js
+++ Praticas Iniciais/Seção [002]: Lógica de progamação/Aula [004] IF - ELSE/script.js
@@ -1,3 +1,5 @@
+// Primeiro MOMENTO da aula \\
+
 /*
 Entre 0 - 11 Bom dia
 Entre 12 - 17 Boa tarde
@@ -9,14 +11,18 @@ Entre 18 - 23 Boa noite
 // Posso ter vários else ifs na checagem 
 // So posso ter um else na checagem
 
-const hora = 18;
+// const hora = 18;
 
-if (hora >= 0 && hora <= 11) {
-    console.log(`Bom dia!`)
-} else if (hora >= 12 && hora <= 17) {
-    console.log(`Boa tarde!`);
-} else if (hora >= 18 && hora <= 23) {
-    console.log(`Boa noite`);
-} else{
-    console.log(`Não identificado sua hora, cabeçudo`)
-}
+// if (hora >= 0 && hora <= 11) {
+//     console.log(`Bom dia!`)
+// } else if (hora >= 12 && hora <= 17) {
+//     console.log(`Boa tarde!`);
+// } else if (hora >= 18 && hora <= 23) {
+//     console.log(`Boa noite`);
+// } else{
+//     console.log(`Não identificado sua hora, cabeçudo`)
+// }
+
+// Segundo MOMENTO da aula \\
+
+// Apenas foi visto novamente um pouco do conceito acima com exemplos similares.
