8c5b6f5aaa6b5030971cdf4e690aa19da236c855
Vitor <vitorvvc2005@gmail.com>
2024-02-16 14:09:17 -0300

    Subindo aula [020] While - Do while

---
+++ Praticas Iniciais/Seção [002]: Lógica de progamação/Aula [020]: While - Do while/script.js
@@ -0,0 +1,27 @@
+// let i = 0;
+// while (i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+function random(min, max) {
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+// let rand = random(min, max);
+let rand = 10;
+
+while (rand !== 10){
+    rand = random(min, max);
+    console.log(rand);
+}
+// Diferenca de do while para while é: While checa condicão e executa, do while executa e depois da execução é checado o while
+console.log('####');
+
+do {
+    rand = random(min, max);
+    console.log(rand);
+} while(rand !== 10);
