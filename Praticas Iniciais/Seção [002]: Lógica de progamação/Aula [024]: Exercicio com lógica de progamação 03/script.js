241424acdee8fb40b4a083a07e2f0a39e75c319a
Vitor <vitorvvc2005@gmail.com>
2024-02-16 15:12:28 -0300

    Subindo exercicio [3]

---
+++ Praticas Iniciais/Seção [002]: Lógica de progamação/Aula [024]: Exercicio com lógica de progamação 03/script.js
@@ -0,0 +1,43 @@
+// Escrever uma função que recebe um numero e retorne:
+// Num é divisivel por 3 = Fizz
+// Num é divisivel por 5 = Buzz
+// Num é divisivel por 3 e  5 = FizzBuzz
+// Num não é divisivel por 3 e 5 = Retorne o proprio num
+// Checar se o número é realmente um num
+// Use a função com nums de 0 a 100
+
+// Minha solução:
+const main = (num) => {
+    const check = () => {
+        return typeof num == 'number' ? num : 0;
+    }
+    const numName = () => {
+        if (num % 3 === 0 && num % 5 === 0) {
+            return 'FizzBuzz'
+        } else if (num % 3 === 0) {
+            return 'Fizz';
+        } else if (num % 5 === 0) {
+            return 'Buzz';
+        } else {
+            return num;
+        }
+    }
+    return numName();
+}
+console.log('a', main('a'));
+for (let i = 0; i <= 100; i++) {
+    console.log(i, main(i));
+}
+
+// // Solução professor: 
+// function fizzBuzz(numero) {
+//     if (typeof numero !== 'number') return numero;
+//     if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz'
+//     if (numero % 3 === 0) return 'Fizz';
+//     if (numero % 5 === 0) return 'Buzz';
+//     return numero;
+// }
+// for (let i = 0; i <= 100; i++) {
+//     console.log(i, fizzBuzz(i));
+// }
+// console.log('a', fizzBuzz('a'));
